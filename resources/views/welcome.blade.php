4f83831459540dd5dc65d5a55c9a3d65d0b2efc0
Simon Willan <swillan@gonetcoins.com>
2018-10-15 00:41:03 -0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/app.css"  media="screen,projection"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+        <a class="waves-effect waves-light btn">button</a>
+
+        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+        </button>
+
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/app.js"></script>
+    </body>
+  </html>
